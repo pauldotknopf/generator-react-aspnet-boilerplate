8809c21a162c3cbc101b475a0c8bc70479eccbdb
Paul Knopf <pauldotknopf@gmail.com>
2016-06-18 13:50:36 -0400

    Fixing tests.

--- test/app.js
+++ test/app.js
@@ -6,13 +6,29 @@ var helpers = require('yeoman-test');
 describe('generator-react-aspnet-boilerplate:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts({projectName: 'TestProjectName'})
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt'
+      'TestProjectName.sln',
+      'src/TestProjectName/TestProjectName.xproj'
+    ]);
+  });
+});
+
+describe('generator-react-aspnet-boilerplate:empty-template', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../generators/empty-template'))
+      .withPrompts({projectName: 'TestProjectName'})
+      .on('end', done);
+  });
+
+  it('creates files', function () {
+    assert.file([
+      'TestProjectName.sln',
+      'src/TestProjectName/TestProjectName.xproj'
     ]);
   });
 });
